40099fbfc2736ebc790738a7b2543cf289d9ff06
Vladislav <vbura2000@gmail.com>
2026-02-28 21:45:58 +0300

    Подгрузка задания

---
+++ tests/health.test.js
@@ -0,0 +1,13 @@
+import { getHealthStatus } from "../src/health";
+
+const dataSet = [
+  [{ health: 90, name: "Batman" }, "healthy"],
+  [{ health: 50, name: "Spider-man" }, "wounded"],
+  [{ health: 14, name: "Jocker" }, "critical"],
+];
+
+describe("getHealthStatus", () => {
+  test.each(dataSet)("on data: %j should return %s", (data, expected) => {
+    expect(getHealthStatus(data)).toBe(expected);
+  });
+});
